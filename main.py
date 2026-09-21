631efee40fba4c958e9f21c9ad642d37daaaf91f
Ігор Квасниця <ih.kvasnytsia@gmail.com>
2026-09-21 12:15:04 +0300

    gitignore file

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from controllers.app_controller import AppController
-from models.ascii_data import ASCIIData
-from services.character_search_service import CharacterSearchService
-from services.localization_service import LocalizationService
-from views.main_view import MainView
+import sys
 
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+from views.ui_main_window import MainWindow
 
 def main():
-
-    data = ASCIIData()
-    search_engine = CharacterSearchService(data)
-    localization_service = LocalizationService()
-    app_controller = AppController(search_engine, data, localization_service)
     
-    MainView(app_controller).run()   
+    app = QApplication(sys.argv)
+
+    window = QMainWindow()
+
+    ui = MainWindow()
+    ui.setupUi(window)
+
+    window.show()
+
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
